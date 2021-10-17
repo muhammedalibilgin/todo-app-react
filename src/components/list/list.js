87d98e4f37a18d3030da716f7ce77ad617134d50
Muhammed Ali Bilgin <76842508+muhammedalibilgin@users.noreply.github.com>
2021-10-17 21:57:01 +0300

    finish

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -1,59 +1,79 @@
-import { useState, useEffect } from "react";
+import React from "react";
 
 function List({ todos }) {
-      const [checked, setChecked] = useState(true);
+      const inputDoing = document.getElementsByName("inputDoing");
 
-      useEffect(() => {
-            console.log("change");
-      }, [checked]);
+      function inputControl() {
+            inputDoing.forEach((x, i) => {
+                  if (x.checked) {
+                        inputDoing[i].parentNode.parentNode.classList.add("completed");
+                  } else {
+                        inputDoing[i].parentNode.parentNode.classList.remove("completed");
+                  }
+            });
+      }
+
+      function destroyToDo(i) {
+            console.log("clicked", i);
+
+            inputDoing[i].parentNode.parentNode.classList.add("destroy-display");
+      }
 
       return (
-            <section className="main">
-                  <input className="toggle-all" type="checkbox" />
-                  <label htmlFor="toggle-all"> Mark all as complete </label>
+            <div>
+                  <section className="main">
+                        <input className="toggle-all" type="checkbox" />
+                        <label htmlFor="toggle-all"> Mark all as complete </label>
 
-                  <ul className="todo-list">
-                        {/* <li className="completed">
-                              <div className="view">
-                                    <input className="toggle" type="checkbox" />
-                                    <label>Learn JavaScript</label>
-                                    <button className="destroy"></button>
-                              </div>
-                        </li>
-                        <li>
-                              <div className="view">
-                                    <input className="toggle" type="checkbox" />
-                                    <label>Learn React</label>
-                                    <button className="destroy"></button>
-                              </div>
-                        </li>
-                        <li>
-                              <div className="view">
-                                    <input className="toggle" type="checkbox" />
-                                    <label>Have a life!</label>
-                                    <button className="destroy"></button>
-                              </div>
-                        </li> */}
-                        {todos.map((x, i) => (
-                              <li className="completed" key={i}>
-                                    <div className="view">
-                                          <input
-                                                id="inputCheck"
-                                                onClick={() => {
-                                                      setChecked(!checked);
-                                                      console.log(i);
-                                                }}
-                                                className="toggle"
-                                                type="checkbox"
-                                          />
+                        <ul className="todo-list">
+                              {todos.map((x, i) => (
+                                    <li key={i} id={i}>
+                                          <div className="view">
+                                                <input
+                                                      id="inputCheck"
+                                                      name="inputDoing"
+                                                      onClick={() => inputControl()}
+                                                      className="toggle"
+                                                      type="checkbox"
+                                                />
 
-                                          <label> {x.value} </label>
-                                          <button className="destroy"></button>
-                                    </div>
+                                                <label> {x.value} </label>
+                                                <button className="destroy" onClick={() => destroyToDo(i)}></button>
+                                          </div>
+                                    </li>
+                              ))}
+                        </ul>
+                  </section>
+
+                  <footer className="footer">
+                        <span className="todo-count">
+                              <strong>0 </strong>
+                              yapılacak iş
+                        </span>
+
+                        <ul className="filters">
+                              <li>
+                                    <a href="!#" className="[if(activeFilter = 'all', 'selected')]">
+                                          All
+                                    </a>
                               </li>
-                        ))}
-                  </ul>
-            </section>
+                              <li>
+                                    <a href="!#" className="[if(activeFilter = 'active', 'selected')]">
+                                          Active
+                                    </a>
+                              </li>
+                              <li>
+                                    <a href="!#" className="[if(activeFilter = 'completed', 'selected')]">
+                                          Completed
+                                    </a>
+                              </li>
+                        </ul>
+
+                        <button hidden="[todoDone = 0]" className="clear-completed" mv-action="delete(todo where done)">
+                              Clear completed
+                        </button>
+                  </footer>
+            </div>
       );
 }
 
